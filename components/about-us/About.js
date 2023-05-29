99e8bb353ce8b0b7b2ecafc9828ea7eb38e6d247
Jecy Jehu <jecyjehu@outlook.com>
2023-05-29 19:37:58 +0100

    deploy

--- components/about-us/About.js
+++ components/about-us/About.js
@@ -5,7 +5,7 @@ import Link from "next/link";
 
 const About = () => {
   return (
-    <section className='relative w-full h-auto mt-52'>
+    <section className='relative w-full h-auto mt-20 md:mt-52'>
         <div className='grid md:grid-cols-2 md:items-center max-w-[400px] md:max-w-[1100px] m-auto'>
             <Image className='' src='/assets/images/man.png' alt='/' width={500} height={500} />
             <div className="space-y-10 md:space-y-20">
