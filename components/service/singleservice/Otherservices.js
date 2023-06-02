1e38b872efc35556f73b47230e4eedba0ebcd73e
Jecy Jehu <jecyjehu@outlook.com>
2023-06-02 16:17:09 +0100

    deploy

--- components/service/singleservice/Otherservices.js
+++ components/service/singleservice/Otherservices.js
@@ -1,5 +1,8 @@
 'use client'
 
+
+
+import { useEffect, useState } from 'react';
 import ServiceItem from './ServiceItem';
 import { useRouter } from 'next/navigation';
 
@@ -15,7 +18,15 @@ const Otherservices = () => {
   const router = useRouter();
   const currentRoute = router.route;
 
-  const filteredServices = services.filter(service => service.route !== currentRoute);
+  const [hiddenServices, setHiddenServices] = useState([]);
+
+  // Update hidden services when the current route changes
+  useEffect(() => {
+    const updatedHiddenServices = services.filter(service => service.route === currentRoute);
+    setHiddenServices(updatedHiddenServices);
+  }, [currentRoute]);
+
+  const filteredServices = services.filter(service => !hiddenServices.includes(service));
 
   return (
     <section className="relative md:top-32 w-full h-auto my-20 sm:my-20 md:mb-10 md:my-0">
@@ -32,3 +43,37 @@ const Otherservices = () => {
 };
 
 export default Otherservices;
+
+
+// import ServiceItem from './ServiceItem';
+// import { useRouter } from 'next/navigation';
+
+// const Otherservices = () => {
+//   const services = [
+//     { name: 'Mechanical Engineering', route: '/services/mechanical' },
+//     { name: 'Chemical Engineering', route: '/services/chemical' },
+//     { name: 'Civil Engineering', route: '/services/civil' },
+//     { name: 'Computer Engineering', route: '/services/computer' },
+//     { name: 'Petroleum Engineering', route: '/services/petroleum' }
+//   ];
+
+//   const router = useRouter();
+//   const currentRoute = router.route;
+
+//   const filteredServices = services.filter(service => service.route !== currentRoute);
+
+//   return (
+//     <section className="relative md:top-32 w-full h-auto my-20 sm:my-20 md:mb-10 md:my-0">
+//       <div className="max-w-[400px] sm:max-w-[600px] md:max-w-[1000px] mx-auto">
+//         <h1 className='text-2xl md:text-4xl font-extrabold mb-10'>Other Services</h1>
+//         <div className='grid grid-cols-2 grid-rows-2 gap-2 sm:grid-cols-3 md:grid-cols-4'>
+//           {filteredServices.map((service, index) => (
+//             <ServiceItem key={index} serviceName={service.name} route={service.route} />
+//           ))}
+//         </div>
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default Otherservices;
--- components/service/singleservice/ServiceItem.js
+++ components/service/singleservice/ServiceItem.js
@@ -7,6 +7,7 @@ import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 const ServiceItem = ({ serviceName, route }) => {
   const router = useRouter();
   const currentRoute = router.route;
+  const isHidden = currentRoute === route;
 
   const handleClick = () => {
     router.push(route);
@@ -14,7 +15,7 @@ const ServiceItem = ({ serviceName, route }) => {
 
   return (
     <>
-      {route !== currentRoute && (
+      {!isHidden && (
         <div className='border border-t-8 border-t-[#D2153D] shadow-2xl px-2 py-5 space-y-4 bg-white'>
           <h2 className='text-lg font-light'>Explore Service</h2>
           <h1 className='text-2xl font-semibold'>{serviceName}</h1>
@@ -30,3 +31,35 @@ const ServiceItem = ({ serviceName, route }) => {
 };
 
 export default ServiceItem;
+
+
+
+// import { useRouter } from 'next/navigation';
+// import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
+
+// const ServiceItem = ({ serviceName, route }) => {
+//   const router = useRouter();
+//   const currentRoute = router.route;
+
+//   const handleClick = () => {
+//     router.push(route);
+//   };
+
+//   return (
+//     <>
+//       {route !== currentRoute && (
+//         <div className='border border-t-8 border-t-[#D2153D] shadow-2xl px-2 py-5 space-y-4 bg-white'>
+//           <h2 className='text-lg font-light'>Explore Service</h2>
+//           <h1 className='text-2xl font-semibold'>{serviceName}</h1>
+//           <div>
+//             <button className='flex items-center justify-between w-16 font-semibold' onClick={handleClick}>
+//               Read <HiOutlineArrowNarrowRight className='text-[#D2153D]' />
+//             </button>
+//           </div>
+//         </div>
+//       )}
+//     </>
+//   );
+// };
+
+// export default ServiceItem;
