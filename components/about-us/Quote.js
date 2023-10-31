18d5e3c2647b3a2f76e8f27f2462c95085fcee3a
Jecy Jehu <jecyjehu@outlook.com>
2023-10-31 19:23:09 +0100

    deploy

--- components/about-us/Quote.js
+++ components/about-us/Quote.js
@@ -12,26 +12,26 @@ const Quote = () => {
             <p className="text-white text-xs md:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
             <Button>Appointment</Button>
         </div>
-        <div className="relative top-7 md:top-80 grid grid-cols-2 md:grid-cols-3 max-w-[1000px] m-auto">
-            <div className="flex bg-white space-x-2 max-w-[280px]">
+        <div className="relative top-7 md:top-40 lg:top-80 grid grid-cols-2 md:grid-cols-3 max-w-[1000px] m-auto px-3 md:px-5">
+            <div className="flex bg-white space-x-2 max-w-[220px]">
                 <h1 className="text-white text-center w-14 h-14 font-semibold text-2xl bg-[#D2153D] p-3 ">01</h1>
                 <div>
-                    <h2 className="text-[#00215B] text-lg font-bold">Kind Consultation</h2>
-                    <p className="text-sm md:text-base">Etiam ac leo at quam aliquet</p>
+                    <h2 className="text-[#00215B] font-bold">Kind Consultation</h2>
+                    <p className="text-xs sm:text-sm lg:text-base">Etiam ac leo at quam aliquet</p>
                 </div>
             </div>
-            <div className="flex bg-white space-x-2 max-w-[280px]">
+            <div className="flex bg-white space-x-2 max-w-[220px]">
                 <h1 className="text-white text-center w-14 h-14 font-semibold text-2xl bg-[#D2153D] p-3 ">02</h1>
                 <div>
-                    <h2 className="text-[#00215B] text-lg font-bold">Kind Consultation</h2>
-                    <p className="text-sm md:text-base">Etiam ac leo at quam aliquet</p>
+                    <h2 className="text-[#00215B] font-bold">Kind Consultation</h2>
+                    <p className="text-xs sm:text-sm lg:text-base">Etiam ac leo at quam aliquet</p>
                 </div>
             </div>
             <div className="hidden md:flex bg-white space-x-2 max-w-[280px]">
                 <h1 className="text-white text-center w-14 h-14 font-semibold text-2xl bg-[#D2153D] p-3 ">03</h1>
                 <div>
-                    <h2 className="text-[#00215B] text-lg font-bold">Kind Consultation</h2>
-                    <p className="text-sm md:text-base">Etiam ac leo at quam aliquet</p>
+                    <h2 className="text-[#00215B] font-bold">Kind Consultation</h2>
+                    <p className="text-sm sm:text-sm lg:text-base">Etiam ac leo at quam aliquet</p>
                 </div>
             </div>
         </div>
