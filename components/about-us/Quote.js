a009eed7ec52e9ae1fbd57c979056520c892c13b
Jecy Jehu <jecyjehu@outlook.com>
2023-10-31 19:43:44 +0100

    deploy

--- components/about-us/Quote.js
+++ components/about-us/Quote.js
@@ -6,32 +6,32 @@ const Quote = () => {
   return (
     <section className="relative w-full h-auto md:h-screen my-20 md:my-0 md:mt-20">
         <Image className="absolute" src='/assets/images/london-bridge.png' alt="/" width={1500} height={1200} />
-        <div className="relative top-2 md:top-36 md:max-w-[800px] mx-auto text-center space-y-2">
+        <div className="relative top-2 md:top-20 lg:top-40 md:max-w-[800px] mx-auto text-center space-y-2">
             <h2 className="text-white text-base md:text-lg">Get a Quote</h2>
             <h1 className="text-white text-2xl md:text-3xl font-semibold">Build Your Future Today</h1>
-            <p className="text-white text-xs md:text-base">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,</p>
+            <p className="text-white text-xs md:text-base">Our Vision is to build a better Structure to satisfy your needs.</p>
             <Button>Appointment</Button>
         </div>
-        <div className="relative top-7 md:top-40 lg:top-80 grid grid-cols-2 md:grid-cols-3 max-w-[1000px] m-auto px-3 md:px-5">
+        <div className="relative top-7 md:top-36 lg:top-[380px] grid grid-cols-2 md:grid-cols-3 max-w-[1000px] m-auto px-3 md:px-5">
             <div className="flex bg-white space-x-2 max-w-[220px]">
                 <h1 className="text-white text-center w-14 h-14 font-semibold text-2xl bg-[#D2153D] p-3 ">01</h1>
                 <div>
                     <h2 className="text-[#00215B] font-bold">Kind Consultation</h2>
-                    <p className="text-xs sm:text-sm lg:text-base">Etiam ac leo at quam aliquet</p>
+                    <p className="text-xs sm:text-sm lg:text-base">Contact loren Doe</p>
                 </div>
             </div>
             <div className="flex bg-white space-x-2 max-w-[220px]">
                 <h1 className="text-white text-center w-14 h-14 font-semibold text-2xl bg-[#D2153D] p-3 ">02</h1>
                 <div>
                     <h2 className="text-[#00215B] font-bold">Kind Consultation</h2>
-                    <p className="text-xs sm:text-sm lg:text-base">Etiam ac leo at quam aliquet</p>
+                    <p className="text-xs sm:text-sm lg:text-base">Contact ben Doe</p>
                 </div>
             </div>
             <div className="hidden md:flex bg-white space-x-2 max-w-[280px]">
                 <h1 className="text-white text-center w-14 h-14 font-semibold text-2xl bg-[#D2153D] p-3 ">03</h1>
                 <div>
                     <h2 className="text-[#00215B] font-bold">Kind Consultation</h2>
-                    <p className="text-sm sm:text-sm lg:text-base">Etiam ac leo at quam aliquet</p>
+                    <p className="text-sm sm:text-sm lg:text-base">Contact Hannah Doe</p>
                 </div>
             </div>
         </div>
--- components/reusable files/Button.js
+++ components/reusable files/Button.js
@@ -3,7 +3,7 @@
 const Button = ({onClick, children}) => {
   return (
     <section>
-        <button onClick={onClick} className="bg-[#D2153D] text-white py-2 w-52 rounded-lg">
+        <button onClick={onClick} className="bg-[#D2153D] text-white py-1 w-52 rounded-lg">
             {children}
         </button>
     </section>
