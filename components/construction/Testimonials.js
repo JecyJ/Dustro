366fc3dbd8c6048e20e935f437636a7eb30a5559
Jecy Jehu <jecyjehu@outlook.com>
2023-10-31 16:40:46 +0100

    deploy

--- components/construction/Testimonials.js
+++ components/construction/Testimonials.js
@@ -2,30 +2,32 @@
 
 import Image from 'next/image';
 import {FaQuoteRight} from 'react-icons/fa'
-import {BsArrowLeftCircle, BsArrowRightCircle} from 'react-icons/bs'
-import { useRef, useState } from 'react';
+// import {BsArrowLeftCircle, BsArrowRightCircle} from 'react-icons/bs'
+// import { useRef, useState } from 'react';
 import TestimonialsData from './TestimonialsData';
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 
 const Testimonials = () => {
-  const sliderRef = useRef(null);
-  const [sliderPosition, setSliderPosition] = useState(0);
+  // const sliderRef = useRef(null);
+  // const [sliderPosition, setSliderPosition] = useState(0);
 
-  const handlePrev = () => {
-    const slideWidth = sliderRef.current.offsetWidth;
-    const newPosition = Math.min(sliderPosition + slideWidth, 0);
-    setSliderPosition(newPosition);
-  };
+  // const handlePrev = () => {
+  //   const slideWidth = sliderRef.current.offsetWidth;
+  //   const newPosition = Math.min(sliderPosition + slideWidth, 0);
+  //   setSliderPosition(newPosition);
+  // };
 
-  const handleNext = () => {
-    const slideWidth = sliderRef.current.offsetWidth;
-    const numItems = TestimonialsData.length;
-    const sliderWidth = slideWidth * numItems;
-    const newPosition = Math.max(sliderPosition - slideWidth, -sliderWidth);
-    setSliderPosition(newPosition);
+  // const handleNext = () => {
+  //   const slideWidth = sliderRef.current.offsetWidth;
+  //   const numItems = TestimonialsData.length;
+  //   const sliderWidth = slideWidth * numItems;
+  //   const newPosition = Math.max(sliderPosition - slideWidth, -sliderWidth);
+  //   setSliderPosition(newPosition);
 
-    // setCurrentItemIndex((prevIndex) => prevIndex + 1);
-  };
+  //   // setCurrentItemIndex((prevIndex) => prevIndex + 1);
+  // };
 
 
   return (
@@ -33,59 +35,71 @@ const Testimonials = () => {
         <div className='max-w-[450px] md:max-w-[1000px] mx-auto text-center space-y-5 md:space-y-7'>
           <h2 className='text-[#D2153D]'>TESTIMONIALS</h2>
           <h1 className='text-3xl md:text-5xl font-semibold'>Our Clients Say</h1>
-          <div className='relative flex flex-col'>
-            <button 
-              onClick={handlePrev} 
-              className="absolute z-[2] top-1/2 text-[#00215B] transform -translate-y-1/2 left-0"
-              disabled={sliderPosition >= 0}
-            >
-              <BsArrowLeftCircle size={30} />
-            </button>
-
-            <div className="relative w-full overflow-hidden">
-              <div
-                className="flex transition-transform duration-500 ease-in"
-                style={{ transform: `translateX(${sliderPosition}px)` }}
-                ref={sliderRef}
-              >
-                {TestimonialsData.map((item) => (
-                  <div key={item.id} className="flex-shrink-0 w-full p-4">
-                    <div className="flex items-center w-full">
-                      <Image
-                        className="h-5/6 md:h-96 w-52 md:w-80"
-                        src={item.image}
-                        width={500}
-                        height={500}
-                      />
-                      <div className="space-y-1 md:space-y-6">
-                        <FaQuoteRight
-                          className="border w-10 md:w-16 h-10 md:h-16 p-2 md:p-5 text-white bg-[#00215B]"
-                          size={10}
-                        />
-                        <p className="text-sm md:text-xl">{item.paragraph}</p>
-                        <div className="text-left">
-                          <h1 className="font-semibold text-xl md:text-3xl">{item.name}</h1>
-                          <h2 className="text-[#00215B] text-sm md:text-2xl font-medium">{item.title}</h2>
-                        </div>
-                      </div>
+          <Splide options={{
+            perPage:1,
+            arrows:true,
+            pagination: false,
+            drag:"free",
+          }}
+            className="transition-transform duration-500 ease-in"                
+          >
+            {TestimonialsData.map((item) => (
+              <SplideSlide key={item.id} className="w-full px-12">
+                <div className="flex items-center w-full">
+                  <Image
+                    className="h-5/6 md:h-96 w-52 md:w-80"
+                    src={item.image}
+                    width={500}
+                    height={500}
+                  />
+                  <div className="space-y-1 md:space-y-6 text-left">
+                    <FaQuoteRight
+                      className="border w-10 md:w-16 h-10 md:h-16 p-2 md:p-5 text-white bg-[#00215B]"
+                      size={10}
+                    />
+                    <p className="text-sm md:text-xl">{item.paragraph}</p>
+                    <div>
+                      <h1 className="font-semibold text-xl md:text-3xl">{item.name}</h1>
+                      <h2 className="text-[#00215B] text-sm md:text-2xl font-medium">{item.title}</h2>
                     </div>
                   </div>
-                ))}
-              </div>
-            </div>        
-
-
-            <button 
-              onClick={handleNext} 
-              className="absolute z-[2] top-1/2 text-[#00215B] transform -translate-y-1/2 right-0"
-              // disabled={currentItemIndex === TestimonialsData.length - 1}
-            >
-              <BsArrowRightCircle size={30} />
-            </button>
-          </div>
+                </div>
+              </SplideSlide>
+            ))}
+          </Splide>          
         </div>
     </section>    
   )
 }
 
 export default Testimonials;
+
+// <button 
+//   onClick={handlePrev} 
+//   className="absolute z-[2] top-1/2 text-[#00215B] transform -translate-y-1/2 left-0"
+//   disabled={sliderPosition >= 0}
+// >
+//   <BsArrowLeftCircle size={30} />
+// </button>
+
+
+// <button 
+// onClick={handleNext} 
+// className="absolute z-[2] top-1/2 text-[#00215B] transform -translate-y-1/2 right-0"
+// // disabled={currentItemIndex === TestimonialsData.length - 1}
+// >
+// <BsArrowRightCircle size={30} />
+// </button>
+
+// style={{ transform: `translateX(${sliderPosition}px)` }}
+//                 ref={sliderRef}
+
+
+// <div className='relative flex flex-col'>
+//             <div className="relative w-full overflow-hidden">
+              
+//             </div>        
+
+
+
+//           </div>
--- components/construction/TestimonialsData.js
+++ components/construction/TestimonialsData.js
@@ -2,21 +2,21 @@ const TestimonialsData = [
     {
         id:'1',
         image: '/assets/images/handshake.png',
-        paragraph: 'Suspendisse tortor enim, varius et porttitor sit amet, posuere vitae massa. Proin ac quam eu erat semper sagittis in vitae elit. Nam neque erat, semper vel ultrices in, finibus eu magna. Pellentesque habitant morbi tristique',
+        paragraph: "We've deemed Dustro as our committed partner to help build stores. It's great to have a partner like Dustro to be able to feel confident that what you're building and what you're creating is going to be successful for the next 50 to 150 years or whatever it might be.",
         name: 'Katy Grey',
         title: 'CEO Grey Industries'
     },
     {
         id:'2',
         image: '/assets/images/city.png',
-        paragraph: 'Suspendisse tortor enim, varius et porttitor sit amet, posuere vitae massa. Proin ac quam eu erat semper sagittis in vitae elit. Nam neque erat, semper vel ultrices in, finibus eu magna. Pellentesque habitant morbi tristique',
+        paragraph: "Cooperation between Dustro and the school exceeded our expectations, we were really worried about drop off and end of the day dismissal but honestly, if anything, its been more efficient this year than it was last year.",
         name: 'Jane Doe',
         title: 'CEO Jane Industries'
     },
     {
         id:'3',
         image: '/assets/images/building.png',
-        paragraph: 'Suspendisse tortor enim, varius et porttitor sit amet, posuere vitae massa. Proin ac quam eu erat semper sagittis in vitae elit. Nam neque erat, semper vel ultrices in, finibus eu magna. Pellentesque habitant morbi tristique',
+        paragraph: "I've been very impressed with Dustro's performance on this project. They are well-organized and run a clean, efficient job-site… James Doe and Tom Doe have been great to work with.",
         name: 'John Doe',
         title: 'CEO Doe Industries'
     }
