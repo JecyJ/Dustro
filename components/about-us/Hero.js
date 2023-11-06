e3eab41e4e458cd5899211aecbdd2979e0e16468
Jecy Jehu <jecyjehu@outlook.com>
2023-11-06 12:24:56 +0100

    deploy

--- components/about-us/Hero.js
+++ components/about-us/Hero.js
@@ -9,7 +9,7 @@ const Hero = () => {
         <h1 className="text-3xl md:text-5xl font-semibold">About Us</h1>
         <h3 className="text-xs md:text-lg">Home / About Us</h3>
       </div>
-      <div className="relative w-48 md:w-72 p-3 top-7 sm:top-12 md:top-28 lg:top-48 left-56 sm:left-2/4 md:left-2/3 lg:left-2/3 bg-white border shadow-2xl border-t-4 border-t-[#D2153D]">
+      <div className="relative w-48 md:w-72 p-3 top-7 sm:top-12 md:top-20 lg:top-48 left-56 sm:left-2/4 md:left-2/3 lg:left-2/3 bg-white border shadow-2xl border-t-4 border-t-[#D2153D]">
         <h3 className="text-xs md:text-base">CALL US TODAY</h3>
         <h1 className="text-xl md:text-3xl font-semibold text-[#00215B]">+1 123 4567 789</h1>
       </div>
